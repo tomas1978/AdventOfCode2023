c695e66c73f9a460381e002d97d814db18af88a4
tomas1978 <tomas.nilsson78@gmail.com>
2023-12-06 10:23:17 +0100

    Started day 2 problem 1
    Opening and reading from text file

---
+++ day2_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    ifstream infile("input.txt");
+    for(string line; getline(infile, line); )
+    {
+        
+    }
+}
