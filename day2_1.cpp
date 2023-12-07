64ff63844050e7f23662494573c4332991a543e9
tomas1978 <tomas.nilsson78@gmail.com>
2023-12-07 23:19:11 +0100

    Experimenting with reading from file

--- day2_1.cpp
+++ day2_1.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main()
 {
     ifstream infile("input.txt");
-    for(string line; getline(infile, line); )
+    //for(string line; getline(infile, line, ' ' ); )
+    string input;
+    while(true)
     {
-        
+        getline(infile, input, ' ');
+        cout<<input<<endl;
     }
 }
