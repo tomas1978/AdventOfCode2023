e48280b5b4af21173072e426f7f11446760093f5
tomas1978 <tomas.nilsson78@gmail.com>
2023-12-18 14:32:17 +0100

    Trying a new implementation of findFirstDigit

--- day1_2_v3.cpp
+++ day1_2_v3.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string>
 #include<fstream>
 #include<vector>
 #include<algorithm>
@@ -9,12 +8,10 @@ int findLastDigit(std::string s)
     std::string from_end="";
     for(int i=s.size()-1;i>=0;i--)
     {
-       std::string temp="";
-       from_end.push_back(s[i]);
-       //std::string temp=from_end;
+        std::string temp="";
+        from_end.push_back(s[i]);
 
-       std::reverse(temp.begin(),temp.end());
-        //std::cout<<temp<<std::endl;
+        std::reverse(temp.begin(),temp.end());
         if(temp.find("one")>0 || temp.find("1")>0)
             return 1;
         if(temp.find("two")>0 || temp.find("2")>0)
@@ -42,56 +39,36 @@ int findLastDigit(std::string s)
 //substring "one", "two", ..., "nine" in input string
 int findFirstDigit(std::string s) 
 {
-    int positions[]={0,0,0,0,0,0,0,0,0};
-    if(s.find("one")<s.find("1"))
-        positions[0]=s.find("one");
-    if(s.find("1")<=s.find("one"))
-        positions[0]=s.find("1");
-    if(s.find("two")<s.find("2"))
-        positions[1]=s.find("two");
-    if(s.find("2")<=s.find("two"))
-        positions[1]=s.find("2");
-    if(s.find("three")<s.find("3"))
-        positions[2]=s.find("three");
-    if(s.find("3")<=s.find("three"))
-        positions[2]=s.find("3");
-    if(s.find("four")<s.find("4"))
-        positions[3]=s.find("four");
-    if(s.find("4")<=s.find("four"))
-        positions[3]=s.find("4");
-    if(s.find("five")<s.find("5"))
-        positions[4]=s.find("five");
-    if(s.find("5")<=s.find("five"))
-        positions[4]=s.find("5");
-    if(s.find("six")<s.find("6"))
-        positions[5]=s.find("six");
-    if(s.find("6")<=s.find("six"))
-        positions[5]=s.find("6");
-    if(s.find("seven")<s.find("7"))
-        positions[6]=s.find("seven");
-    if(s.find("7")<=s.find("seven"))
-        positions[6]=s.find("7");
-    if(s.find("eight")<s.find("8"))
-        positions[7]=s.find("eight");
-    if(s.find("8")<=s.find("eight"))
-        positions[7]=s.find("8");
-    if(s.find("nine")<s.find("9"))
-        positions[8]=s.find("nine");
-    if(s.find("9")<=s.find("nine"))
-        positions[8]=s.find("9");
-    
-    for(int i=0;i<9;i++)
-        std::cout<<positions[i]<<",";
-    
-    std::cout<<"Position of one: "<<positions[0]<<std::endl;
+    std::string from_beginning="";
+    for(int i=s.size()-1;i>=0;i--)
+    {
+        if(from_beginning.find("one")>0 || from_beginning.find("1")>0)
+            return 1;
+        if(from_beginning.find("two")>0 || from_beginning.find("2")>0)
+            return 2;
+        if(from_beginning.find("three")>0 || from_beginning.find("3")>0)
+            return 3;
+        if(from_beginning.find("four")>0 || from_beginning.find("4")>0)
+            return 4;
+        if(from_beginning.find("five")>0 || from_beginning.find("5")>0)
+            return 5;
+        if(from_beginning.find("six")>0 || from_beginning.find("6"))
+            return 6;
+        if(from_beginning.find("seven") || from_beginning.find("7"))
+            return 7;
+        if(from_beginning.find("eight")>0 || from_beginning.find("8")>0)
+            return 8;
+        if(from_beginning.find("nine")>0 || from_beginning.find("9")>0)
+            return 9;
+    }
     return -1;
 }
 
 int main()
 {
-    std::string test="fgthree2sixytoneptwoo";
-    //std::cout<<findFirstDigit(test)<<std::endl;
-    std::cout<<findLastDigit(test)<<std::endl;
+    std::string test="6fgthree2sixytoneptwoo";
+    std::cout<<findFirstDigit(test)<<std::endl;
+    //std::cout<<findLastDigit(test)<<std::endl;
 
 
 
@@ -106,10 +83,10 @@ int main()
     for(std::string line; getline(infile, line); )
     {
         std::string firstLastDigit;
-        firstLastDigit.push_back(findFirstDigit(line));
-        firstLastDigit.push_back(findLastDigit(line));
-        int value=stoi(firstLastDigit);
-        sum+=value;
+        //firstLastDigit.push_back(findFirstDigit(line));
+        //firstLastDigit.push_back(findLastDigit(line));
+        //int value=stoi(firstLastDigit);
+        //sum+=value;
     }
 
     //std::cout<<"Sum of all calibration values: "<<sum<<std::endl;
